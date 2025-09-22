9e8708d0f1ac08f4f37ecf3c85bb06f9970fc1f4
MLGTASTICa <61350382+MLGTASTICa@users.noreply.github.com>
2025-09-22 13:12:28 +0300

    Update SharedOxydProjectileSystem.cs

--- Content.Shared/_Oxyd/Guns/SharedOxydProjectileSystem.cs
+++ Content.Shared/_Oxyd/Guns/SharedOxydProjectileSystem.cs
@@ -1,9 +1,15 @@
 
 /// </summary>
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Numerics;
+using Content.Shared._Oxyd.Framework;
+using Content.Shared.Damage;
 using Content.Shared.Weapons.Ranged.Components;
 using Robust.Shared.Map;
+using Robust.Shared.Network;
+using Robust.Shared.Physics;
+using Robust.Shared.Physics.Events;
 using Robust.Shared.Physics.Systems;
 using Robust.Shared.Serialization;
 using Robust.Shared.Timing;
@@ -30,6 +36,8 @@ public abstract class SharedOxydProjectileSystem : EntitySystem
     [Dependency] protected readonly SharedTransformSystem _transform = default!;
     [Dependency] protected readonly SharedOxydGunSystem _Guns = default!;
     [Dependency] protected readonly SharedPhysicsSystem _physics = default!;
+    [Dependency] protected readonly INetManager _netmanager = default!;
+    [Dependency] protected readonly DamageableSystem _damage = default!;
     //[Dependency] private readonly EntityManager _entityManager = default!;
     public List<Entity<OxydProjectileComponent>> Projectiles = new List<Entity<OxydProjectileComponent>>();
     public List<Entity<OxydProjectileComponent>> FireNextTick =  new List<Entity<OxydProjectileComponent>>();
@@ -39,6 +47,33 @@ public abstract class SharedOxydProjectileSystem : EntitySystem
     {
         base.Initialize();
         tickDelay = 1000.0f/(float)_gameTiming.TickRate;
+        SubscribeLocalEvent<OxydProjectileComponent, StartCollideEvent>(onCollide);
+    }
+
+    public virtual bool shouldTriggerCollide(Entity<OxydProjectileComponent> obj, ref StartCollideEvent args)
+    {
+        if (!TryComp<FixturesComponent>(obj, out var fixtures))
+            return false;
+        if (args.OurFixture != fixtures.Fixtures.Values.First())
+            return false;
+        return false;
+    }
+
+
+    public virtual void afterBulletCollide(Entity<OxydProjectileComponent> obj, ref StartCollideEvent args)
+    {
+        QueueDel(obj);
+    }
+
+    public void onCollide(Entity<OxydProjectileComponent> obj, ref StartCollideEvent args)
+    {
+        if(!shouldTriggerCollide(obj, ref args))
+            return;
+        if (TryComp<OxydProjectileApplyDamageComponent>(obj, out var damage))
+            _damage.TryChangeDamage(args.OtherEntity, damage.DamageSpecifier, false, true);
+        afterBulletCollide(obj, ref args);
+
+
     }
 
     public void queueProjectile(Entity<OxydProjectileComponent> projectile)
