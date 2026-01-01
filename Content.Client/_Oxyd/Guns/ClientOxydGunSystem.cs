095b1a01d27a418521412c3e870ba6110a3ec922
MLGTASTICa <61350382+MLGTASTICa@users.noreply.github.com>
2026-01-01 08:50:15 +0200

    Update ClientOxydGunSystem.cs

--- Content.Client/_Oxyd/Guns/ClientOxydGunSystem.cs
+++ Content.Client/_Oxyd/Guns/ClientOxydGunSystem.cs
@@ -66,6 +66,12 @@ public sealed partial class ClientOxydGunSystem : SharedOxydGunSystem
             {
                 firemodeSwitchButton,
                 gunSafetyButton,
+                new TextureButton()
+                {
+                    TexturePath = "/Textures/Oxyd/erisported/gunactions16.rsi/safety1.png",
+                    MinSize = new Vector2(32, 32),
+                    MaxSize = new Vector2(32, 32)
+                }
             }
         };
         args.Controls.Add(adding);
