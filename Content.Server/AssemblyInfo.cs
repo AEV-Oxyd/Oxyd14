9ec0565615ce430d932b3ac056f76e0633ceefdc
MLGTASTICa <61350382+MLGTASTICa@users.noreply.github.com>
2026-02-27 23:28:24 +0200

    slop

--- Content.Server/AssemblyInfo.cs
+++ Content.Server/AssemblyInfo.cs
@@ -1,4 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Content.Tests")]
 [assembly: InternalsVisibleTo("Content.IntegrationTests")]
+
+
+[assembly: SuppressMessage("Compiler", "CS0414", Justification = "Development")]
---
+++ Content.Shared/GlobalSuppresions.cs
@@ -0,0 +1,7 @@
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "RA0003", Justification = "Temporarily disabled")]
+[assembly: SuppressMessage("Usage", "RA0033", Justification = "Temporarily disabled")]
+[assembly: SuppressMessage("Usage", "RA0037", Justification = "Temporarily disabled")]
+[assembly: SuppressMessage("Usage", "RA0038", Justification = "Temporarily disabled")]
+[assembly: SuppressMessage("Compiler", "CS0414", Justification = "Temporarily disabled")]
