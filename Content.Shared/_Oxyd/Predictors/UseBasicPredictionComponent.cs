1be920d1ba0e3e0f8ba34dcf0ff576a643a44d28
MLGTASTICa <61350382+MLGTASTICa@users.noreply.github.com>
2026-03-21 17:56:46 +0200

    aaa

--- Content.Shared/_Oxyd/Predictors/UseBasicPredictionComponent.cs
+++ Content.Shared/_Oxyd/Predictors/UseBasicPredictionComponent.cs
@@ -1,10 +1,12 @@
+using Robust.Shared.GameStates;
+
 namespace Content.Shared._Oxyd.Predictors;
 
 /// <summary>
 /// This is used for...
 /// </summary>
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class UseBasicPredictionComponent : Component
 {
-    
+
 }
